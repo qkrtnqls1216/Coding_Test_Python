b0c4f2c1f7a6a20dbe1febb18eecc7a84259d7b4
qkrtnqls1216 <79692357+qkrtnqls1216@users.noreply.github.com>
2023-07-12 11:20:27 +0900

    [level 1] Title: 약수의 합, Time: 0.17 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -0,0 +1,6 @@
+def solution(n):
+    answer = 0 # 약수값 초기화
+    for i in range (1,n+1): # 1~n+1전까지 반복
+        if(n%i == 0): # 0이면 리스트의 끝에 추가
+            answer += i # 약수 저장
+    return answer # 답 반환
