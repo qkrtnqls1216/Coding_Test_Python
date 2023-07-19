62e12cef6594480e583b96e1cbbb17cf0ca09c1c
qkrtnqls1216 <79692357+qkrtnqls1216@users.noreply.github.com>
2023-07-19 09:41:15 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.42 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -3,12 +3,12 @@ def solution(left, right):
     for i in range(left, right + 1):
         divisors = 0  # 약수의 개수를 저장하는 변수
         for j in range(1, i + 1):
-            if i % j == 0:
-                divisors += 1
+            if i % j == 0: # 약수인 경우
+                divisors += 1 # 약수저장하는 divisors에 저장
 
-        if divisors % 2 == 0:
-            answer += i
-        else:
+        if divisors % 2 == 0: # 약수의 개수가 짝수인 경우
+            answer += i 
+        else: # 약수의 개수가 홀수인 경우
             answer -= i
 
     return answer
